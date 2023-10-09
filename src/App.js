705365e19a1a28c51f829f49352b4cdd386c824e
MarcAnthony-WebDev <marcanthony.webdev@gmail.com>
2023-10-08 20:50:36 -0400

    final game

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import HomePage from './components/HomePage/Homepage';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <HomePage />
+      </div>
+    </>
+  );
+}
+
+export default App;
